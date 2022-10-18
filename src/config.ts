840e7f3169779d9c0ba061060cd1c400fc175bbe
0xMahabub <mahabub212002072@gmail.com>
2022-10-18 17:16:07 +0600

    initial-repo

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place all global data in here
+// We can import these data anywhere in our site using `import` keyword
+
+export const SITE_TITLE: string = "DevNotes;";
+export const SITE_DESCRIPTION: string =
+  "An open-source platform for developer's blogging";
