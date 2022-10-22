13d2ea91c3fd94d2caf216e9e4ced51b15cfade6
0xMahabub <mahabub212002072@gmail.com>
2022-10-23 01:28:56 +0600

    cms:pipe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,35 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import CMS from 'astro-netlify-cms';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [
+    mdx(),
+    sitemap(),
+    CMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main'
+        },
+        collections: [
+          {
+            name: 'note',
+            label: 'Notes',
+            folder: 'src/pages/note',
+            create: true,
+            delete: true,
+            editor: true,
+            fields: [
+              { name: 'title', widget: 'string', label: 'Note Name or Title' },
+              { name: 'content', widget: 'markdown', label: 'Write Note' }
+            ]
+          }
+        ]
+      }
+    })
+  ]
 });
